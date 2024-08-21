809cb38c26b62d0bf2ec623047fce5fbc4fbd507
Marco <marco.mangan@gmail.com>
2024-08-21 15:52:36 +0000

    Diagram!

---
+++ day03/account/AccountTest.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class AccountTest
+{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Account account1 = new Account("Jane Green", 50.00);
+        Account account2 = new Account("John Blue", -7.53);
+
+System.out.printf();
+
+    }
+}
